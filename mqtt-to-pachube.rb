36bfefe5b498a4372ba0c21e56619787ddba7a22
Nicholas Humfrey <njh@aelius.com>
2012-01-14 11:16:15 +0000

    Reduced timeout values to 10 seconds and added catching of timeouts

--- mqtt-to-pachube.rb
+++ mqtt-to-pachube.rb
@@ -11,15 +11,20 @@ raise "Error: PACHUBE_API_KEY is not set" if ENV['PACHUBE_API_KEY'].nil?
 def update_datastream_value(feed, datastream, value)
   uri = URI.parse("http://api.pachube.com/v2/feeds/#{feed}/datastreams/#{datastream}")
 
-  response = Net::HTTP.start(uri.host, uri.port) do |http|
-    req = Net::HTTP::Put.new(uri.request_uri)
-    req['X-PachubeApiKey'] = ENV['PACHUBE_API_KEY']
-    req['Content-Type'] = 'application/json'
-    req.body = {'current_value' => value}.to_json
-    http.request(req)
+  begin
+    response = Net::HTTP.start(uri.host, uri.port) do |http|
+      http.read_timeout = 10
+      http.open_timeout = 10
+      req = Net::HTTP::Put.new(uri.request_uri)
+      req['X-PachubeApiKey'] = ENV['PACHUBE_API_KEY']
+      req['Content-Type'] = 'application/json'
+      req.body = {'current_value' => value}.to_json
+      http.request(req)
+    end
+    puts " * #{response.message}"
+  rescue Timeout::Error
+    puts " * Timeout"
   end
-
-  puts " * #{response.message}"
 end
 
 
